dbd04486e2406d987ca47bbe6070e75845223f0d
george-593 <63100333+george-593@users.noreply.github.com>
2023-10-18 19:49:03 +0100

    Automatically create ID's in init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,13 @@
 CREATE TABLE public.inventory (
-    id int PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name varchar(255),
     description varchar(255),
     quantity int
 );
 
 CREATE TABLE public.users (
-    id int PRIMARY KEY,
-    name varchar(255),
-    email varchar(255),
+    id SERIAL PRIMARY KEY,
+    username varchar(255),
     isAdmin boolean,
     password varchar(255)
 );
